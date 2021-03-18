bd782f23a3ae03b95e303750a9d5d867eb1d9c23
Roy1473 <pgajdmga@gmail.com>
2021-03-18 22:00:51 +0900

    memmber型

---
+++ src/types.ts
@@ -0,0 +1,6 @@
+export interface Member {
+  name: string;
+  avatar: string;
+  twitterName: string;
+  githubName: string;
+}
---
+++ src/utils/member.ts
@@ -0,0 +1,8 @@
+import { Member } from "types";
+//blog user information
+export const member: Member = {
+  name: "morihey",
+  avatar: "xxxxx",
+  githubName: "xxxxxx",
+  twitterName: "xxxxxxx",
+};
