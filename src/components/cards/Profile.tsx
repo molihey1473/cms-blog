f1b2de719d9b5a22b2a1a73193de084f75d9d527
Roy1473 <pgajdmga@gmail.com>
2021-03-27 17:29:26 +0900

    profile caerd 編集

--- src/components/cards/Profile.tsx
+++ src/components/cards/Profile.tsx
@@ -1,16 +1,25 @@
 //import { member } from "@src/utils/member";
 import { Member } from "@src/types";
+import { TwitterIcon } from "@src/components/icons/TwitterIcon";
 import styles from "@src/styles/components/profile.module.scss";
-export const Profile: React.FC<{ member: Member }> = ({ member }) => {
+
+export const Profile: React.FC<{ member: Member }> = (props) => {
+  const { avatar, name, bio, twitterName } = props.member;
   return (
     <div className={styles.profile_layout}>
       <div className={styles.profile_content}>
         <div>
-          <img src={member.avatar} alt={member.name} width={250} />
+          <img src={avatar} alt={name} width={250} />
         </div>
-        <h1>{member.name}</h1>
-        <p>{member.bio}</p>
-        <div className={styles.profile_description}></div>
+        <div className={styles.profile_description}>
+          <h1>{name}</h1>
+          <p>{bio}</p>
+          <div className={styles.profile_links}>
+            <a href={`https://twitter.com/${twitterName}`}>
+              <TwitterIcon />
+            </a>
+          </div>
+        </div>
       </div>
     </div>
   );
