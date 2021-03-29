d5a536fde4b12f85ad6a95845e917016a62c1c84
Roy1473 <pgajdmga@gmail.com>
2021-03-29 19:35:35 +0900

    edit PostList

---
+++ src/components/PostList.tsx
@@ -0,0 +1,4 @@
+import { useState } from "react";
+export const PostList: React.FC = () => {
+  return <h1>PostList</h1>;
+};
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,7 +4,9 @@ import { TwitterIcon } from "@src/components/icons/TwitterIcon";
 import { Header } from "@src/components/Header";
 import { Wrapper } from "@src/components/Wrapper";
 import { Profile } from "@src/components/cards/Profile";
+import { PostList } from "@src/components/PostList";
 import { member } from "@src/utils/member";
+import { PostItem } from "@src/types";
 import styles from "@src/styles/pages/Home.module.scss";
 
 const Home: NextPage = () => {
@@ -21,7 +23,9 @@ const Home: NextPage = () => {
             <Profile member={member} />
           </Wrapper>
         </section>
-        <section className={styles.home_posts}></section>
+        <section className={styles.home_posts}>
+          <PostList />
+        </section>
       </main>
     </>
   );
