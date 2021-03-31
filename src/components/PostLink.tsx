495c37c1464a52fe38719be01e5a4dc6087ddf9b
Roy1473 <pgajdmga@gmail.com>
2021-04-01 01:23:06 +0900

    create PostLink.tsx

---
+++ src/components/PostLink.tsx
@@ -0,0 +1,2 @@
+import { PostItem } from "@src/types";
+export const PostLink: React.FC<{ item: PostItem }> = (props) => {};
--- src/components/PostList.tsx
+++ src/components/PostList.tsx
@@ -1,10 +1,15 @@
 import { useState } from "react";
 import { PostItem } from "@src/types";
+import { PostLink } from "@src/components/PostLink";
 export const PostList: React.FC<{ items: PostItem[] }> = (props) => {
   const [postItemCount, setPostItemCount] = useState<number>(32);
   const totalItemCount = props.items?.length || 0;
   if (postItemCount) {
     return <div>記事はありません</div>;
   }
-  return <h1>PostList</h1>;
+  return (
+    <>
+      <div></div>
+    </>
+  );
 };
