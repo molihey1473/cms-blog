379d2120f7bd6ca8575a9a19b18a36a09495aacd
Roy1473 <pgajdmga@gmail.com>
2021-04-01 21:00:00 +0900

    add dayjs

--- src/components/PostLink.tsx
+++ src/components/PostLink.tsx
@@ -1,6 +1,7 @@
 import { PostItem } from "@src/types";
 import Link from "next/link";
 import Image from "next/image";
+import dayjs from "dayjs";
 import styles from "@src/styles/components/PostLink.module.scss";
 export const PostLink: React.FC<{ item: PostItem }> = (props) => {
   const { title, contentSnippet, link, isoDate, dateMiliSeconds } = props.item;
@@ -9,6 +10,7 @@ export const PostLink: React.FC<{ item: PostItem }> = (props) => {
       <a className={styles.post_link_content} href={link}>
         <Image src="/icon/qiita.png" width="30" height="30" alt="qiita" />
         <h2 className={styles.post_link_title}>{title}</h2>
+        <time></time>
       </a>
     </li>
   );
