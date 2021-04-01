79189217c7c0fd4b98175122fa900757005b7ac1
Roy1473 <pgajdmga@gmail.com>
2021-04-01 20:52:17 +0900

    @import responsive.scss

--- src/components/PostLink.tsx
+++ src/components/PostLink.tsx
@@ -1,10 +1,15 @@
 import { PostItem } from "@src/types";
+import Link from "next/link";
+import Image from "next/image";
 import styles from "@src/styles/components/PostLink.module.scss";
 export const PostLink: React.FC<{ item: PostItem }> = (props) => {
   const { title, contentSnippet, link, isoDate, dateMiliSeconds } = props.item;
   return (
-    <article className={styles.post_link}>
-      <h1>{title}</h1>
-    </article>
+    <li className={styles.post_link}>
+      <a className={styles.post_link_content} href={link}>
+        <Image src="/icon/qiita.png" width="30" height="30" alt="qiita" />
+        <h2 className={styles.post_link_title}>{title}</h2>
+      </a>
+    </li>
   );
 };
--- src/components/PostList.tsx
+++ src/components/PostList.tsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import { PostItem } from "@src/types";
 import { PostLink } from "@src/components/PostLink";
+import styles from "@src/styles/components/PostList.module.scss";
 export const PostList: React.FC<{ items: PostItem[] }> = (props) => {
   const [postItemCount, setPostItemCount] = useState<number>(32);
   const totalItemCount = props.items?.length || 0;
@@ -9,11 +10,11 @@ export const PostList: React.FC<{ items: PostItem[] }> = (props) => {
   }
   return (
     <>
-      <div>
+      <ul className={styles.post_list}>
         {props.items.slice(0, postItemCount).map((item, i) => (
           <PostLink key={`post-item-${i}`} item={item} />
         ))}
-      </div>
+      </ul>
     </>
   );
 };
