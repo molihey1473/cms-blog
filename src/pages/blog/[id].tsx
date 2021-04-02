41efb8eab9ee92797d4663952fd1e155df1ef08a
Roy1473 <pgajdmga@gmail.com>
2021-04-02 19:55:52 +0900

    microCMS　テストデータ表示

---
+++ src/pages/blog/[id].tsx
@@ -0,0 +1,42 @@
+import { NextPage, GetStaticPaths, GetStaticProps } from "next";
+const Blog: NextPage = ({ blog }) => {
+  return (
+    <div>
+      <h1>{blog.title}</h1>
+      <p>{blog.publishedAt}</p>
+      <div
+        dangerouslySetInnerHTML={{
+          __html: `${blog.body}`,
+        }}
+      />
+    </div>
+  );
+};
+export const getStaticPaths: GetStaticPaths = async () => {
+  const key = {
+    headers: { "X-API-KEY": process.env.API_KEY },
+  };
+  const data = await fetch("https://roy1473.microcms.io/api/v1/blog", key)
+    .then((res) => res.json())
+    .catch(() => null);
+  const paths = data.contents.map((content) => `/blog/${content.id}`);
+  return {
+    paths,
+    fallback: false,
+  };
+};
+export const getStaticProps: GetStaticProps = async ({ params }) => {
+  const id = params.id;
+  const key = {
+    headers: { "X-API-KEY": process.env.API_KEY },
+  };
+  const data = await fetch("https://roy1473.microcms.io/api/v1/blog/" + id, key)
+    .then((res) => res.json())
+    .catch(() => null);
+  return {
+    props: {
+      blog: data,
+    },
+  };
+};
+export default Blog;
--- src/pages/blog/index.tsx
+++ src/pages/blog/index.tsx
@@ -1,5 +1,32 @@
-import { NextPage } from "next";
-const Blog: NextPage = () => {
-  return <h1>ブログ</h1>;
+import { NextPage, GetStaticProps } from "next";
+import Link from "next/link";
+const BlogItems: NextPage = ({ blog }) => {
+  return (
+    <div>
+      <ul>
+        {blog.map((blog) => (
+          <li key={blog.id}>
+            <Link href={`blog/${blog.id}`}>
+              <a>{blog.title}</a>
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 };
-export default Blog;
+
+export const getStaticProps: GetStaticProps = async () => {
+  const key = {
+    headers: { "X-API-KEY": process.env.API_KEY },
+  };
+  const data = await fetch("https://roy1473.microcms.io/api/v1/blog", key)
+    .then((res) => res.json())
+    .catch(() => null);
+  return {
+    props: {
+      blog: data.contents,
+    },
+  };
+};
+export default BlogItems;
