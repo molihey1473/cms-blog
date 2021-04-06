ea142eccc32f2352c654ce37fcf05ff31476bbf0
Roy1473 <pgajdmga@gmail.com>
2021-04-06 20:48:08 +0900

    Blog -> BlogItem 名前変更

--- src/components/BlogLink.tsx
+++ src/components/BlogLink.tsx
@@ -1,9 +1,9 @@
 //import {NextPage} from 'next'
-import { Blog } from "@src/types";
+import { BlogItem } from "@src/types";
 import Link from "next/link";
 import dayjs from "dayjs";
 import styles from "@src/styles/components/BlogLink.module.scss";
-export const BlogLink: React.FC<{ item: Blog }> = (props) => {
+export const BlogLink: React.FC<{ item: BlogItem }> = (props) => {
   const { id, title, body, publishedAt, category } = props.item;
   return (
     <li className={styles.blog_link}>
