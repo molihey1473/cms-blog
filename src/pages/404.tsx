ac3ae5da1d7b06ff6a1afc4edc4a496232690453
Roy1473 <pgajdmga@gmail.com>
2021-04-08 14:33:29 +0900

    404page修正

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,5 +1,17 @@
 import { NextPage } from "next";
+import { Wrapper } from "@src/components/Wrapper";
+import styles from "@src/styles/pages/Error.module.scss";
 const Error: NextPage = () => {
-  return <h1>404ページ</h1>;
+  return (
+    <>
+      <div className={styles.error_page_layout}>
+        <Wrapper>
+          {" "}
+          <div className={styles.error_status}>404</div>
+          <h1 className={styles.error_message}>Page not found</h1>
+        </Wrapper>
+      </div>
+    </>
+  );
 };
 export default Error;
