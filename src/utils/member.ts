c8e213ba890285d548212be5bf5fdc6a55e90305
Roy1473 <pgajdmga@gmail.com>
2021-06-04 00:42:20 +0900

    profile情報修正

--- src/utils/member.ts
+++ src/utils/member.ts
@@ -4,7 +4,7 @@ export const member: Member = {
   name: "Molihey",
   avatar: "/avatar/molihey.png",
   sources: ["https://qiita.com/Roy1473/feed.atom"],
-  githubName: "Roy1473",
-  twitterName: "roy1473",
+  githubName: "https://github.com/Roy1473",
+  twitterName: "https://twitter.com/roy1473",
   bio: "プログラミング勉強してます。今はReact Next.jsにハマってます。",
 };
