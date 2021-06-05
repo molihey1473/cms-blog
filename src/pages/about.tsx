16c2914c279b7e3c412c0328f8e8c62624e076f9
Roy1473 <pgajdmga@gmail.com>
2021-06-06 00:52:34 +0900

    blog index css

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,9 +1,11 @@
 import Link from "next/link";
 import { NextPage } from "next";
+import { BlogSEO } from "@src/components/BlogSEO";
 
 const About: NextPage = () => {
   return (
     <>
+      <BlogSEO title="About" path="/about" />
       <h1>about</h1>
       <Link href="/">
         <a>back to home</a>
