1ff3b821fcf4e2fa3ef23f0cf841f8f6129b6c68
Roy1473 <pgajdmga@gmail.com>
2021-06-22 18:11:02 +0900

    fix: div layout

--- src/pages/blog/index.tsx
+++ src/pages/blog/index.tsx
@@ -3,22 +3,18 @@ import { getBlogs } from "@src/lib/blog";
 import { BlogItem, TagItems } from "@src/types";
 //import Link from "next/link";
 import { Wrapper } from "@src/components/Wrapper";
-import { BlogLink } from "@src/components/BlogLink";
+import { BlogList } from "@src/components/BlogList";
 import styles from "@src/styles/pages/blog/BlogList.module.scss";
 interface Props {
   blogs: BlogItem[];
 }
-const BlogList: NextPage<Props> = (props) => {
+const page: NextPage<Props> = (props) => {
   return (
     <>
       <main>
         <section className={styles.blog_list_layout}>
           <Wrapper>
-            <div className={styles.blog_list}>
-              {props.blogs.map((blog, i) => (
-                <BlogLink key={`blog-link-${i}`} item={blog} />
-              ))}
-            </div>
+            <BlogList items={props.blogs} />
           </Wrapper>
         </section>
       </main>
@@ -36,4 +32,4 @@ export const getStaticProps: GetStaticProps = async () => {
     },
   };
 };
-export default BlogList;
+export default page;
