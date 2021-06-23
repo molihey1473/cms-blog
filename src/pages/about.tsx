6072b21f03327cd0966a72417f3ba51da4f7ce43
Roy1473 <pgajdmga@gmail.com>
2021-06-23 19:03:04 +0900

    add Wrapper componet in about.tsx

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -2,16 +2,19 @@ import Link from "next/link";
 import { NextPage } from "next";
 import { BlogSEO } from "@src/components/BlogSEO";
 import { CategoryList } from "@src/components/CategoryList";
+import { Wrapper } from "@src/components/Wrapper";
 
 const About: NextPage = () => {
   return (
     <>
       <BlogSEO title="About" path="/about" />
-      <CategoryList />
-      <h1>about</h1>
-      <Link href="/">
-        <a>back to home</a>
-      </Link>
+      <Wrapper>
+        <CategoryList />
+        <h1>about</h1>
+        <Link href="/">
+          <a>back to home</a>
+        </Link>
+      </Wrapper>
     </>
   );
 };
