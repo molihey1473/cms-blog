1a0ff087c0f666fd47eb3ae6f88532a243a61b4b
Roy1473 <pgajdmga@gmail.com>
2021-06-25 23:22:15 +0900

    add: CategoryList component in index.tsx

--- src/pages/blog/index.tsx
+++ src/pages/blog/index.tsx
@@ -4,6 +4,7 @@ import { BlogItem, TagItems } from "@src/types";
 //import Link from "next/link";
 import { Wrapper } from "@src/components/Wrapper";
 import { BlogList } from "@src/components/BlogList";
+import { CategoryList } from "@src/components/CategoryList";
 import styles from "@src/styles/pages/blog/BlogList.module.scss";
 interface Props {
   blogs: BlogItem[];
@@ -12,6 +13,9 @@ const page: NextPage<Props> = (props) => {
   return (
     <>
       <main>
+        <Wrapper>
+          <CategoryList />
+        </Wrapper>
         <section className={styles.blog_list_layout}>
           <Wrapper>
             <BlogList items={props.blogs} />
