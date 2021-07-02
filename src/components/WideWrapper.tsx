cfa619cad60964c0360db5b96015747022984fe3
Roy1473 <pgajdmga@gmail.com>
2021-07-03 05:22:59 +0900

    fix: rename classname

--- src/components/WideWrapper.tsx
+++ src/components/WideWrapper.tsx
@@ -1,4 +1,4 @@
 import styles from "@src/styles/components/Wrapper.module.scss";
 export const WideWrapper: React.FC<{ children: React.ReactNode }> = (props) => {
-  return <div className={styles.layout}>{props.children}</div>;
+  return <div className={styles.wide_wrapper}>{props.children}</div>;
 };
