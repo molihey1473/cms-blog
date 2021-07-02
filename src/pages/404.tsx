186b50d036bacfc9753be024e024d474bdc475e1
Roy1473 <pgajdmga@gmail.com>
2021-07-03 06:35:32 +0900

    remake: Wrapper compomnent

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,15 +1,15 @@
 import { NextPage } from "next";
-import { WideWrapper } from "@src/components/WideWrapper";
+import { Wrapper } from "@src/components/Wrapper";
 import styles from "@src/styles/pages/Error.module.scss";
 const Error: NextPage = () => {
   return (
     <>
       <div className={styles.error_page_layout}>
-        <WideWrapper>
+        <Wrapper>
           {" "}
           <div className={styles.error_status}>404</div>
           <h1 className={styles.error_message}>Page not found</h1>
-        </WideWrapper>
+        </Wrapper>
       </div>
     </>
   );
