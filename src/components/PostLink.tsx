a2f294e6b0a88d9bc2f60388b1d3c3a32f40cbb3
Roy1473 <pgajdmga@gmail.com>
2021-07-03 06:10:35 +0900

    fix layout n add: scss classname

--- src/components/PostLink.tsx
+++ src/components/PostLink.tsx
@@ -9,11 +9,13 @@ export const PostLink: React.FC<{ item: PostItem }> = (props) => {
   return (
     <li className={styles.post_link}>
       <a className={styles.post_link_content} href={link}>
-        <time className={styles.post_link_at} dateTime={isoDate}>
-          {dayjs(isoDate).format("YYYY-MM-DD")}
-        </time>
-        <h2 className={styles.post_link_title}>{title}</h2>
-        <Image src="/icon/qiita.png" width="30" height="30" alt="qiita" />
+        <img src="/icon/qiita.png" width="30" height="30" alt="qiita" />
+        <div className={styles.post_link_details}>
+          <h2 className={styles.post_link_title}>{title}</h2>
+          <time className={styles.post_link_at} dateTime={isoDate}>
+            {dayjs(isoDate).format("YYYY-MM-DD")}
+          </time>
+        </div>
       </a>
     </li>
   );
