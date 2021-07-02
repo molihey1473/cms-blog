3c8c9b0a59403a4964f27a92bd414a8adb7fe841
Roy1473 <pgajdmga@gmail.com>
2021-07-03 06:32:38 +0900

    remode:src/components/WideWrapper.tsx

--- src/components/WideWrapper.tsx
+++
@@ -1,4 +0,0 @@
-import styles from "@src/styles/components/Wrapper.module.scss";
-export const WideWrapper: React.FC<{ children: React.ReactNode }> = (props) => {
-  return <div className={styles.wide_wrapper}>{props.children}</div>;
-};
