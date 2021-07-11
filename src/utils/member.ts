16c716d981947fe0060e609154ac82b55e412d4c
Roy1473 <pgajdmga@gmail.com>
2021-07-12 04:27:32 +0900

    fix: image file

--- src/utils/member.ts
+++ src/utils/member.ts
@@ -2,7 +2,7 @@ import { Member } from "@src/types";
 //blog user information
 export const member: Member = {
   name: "Molihey",
-  avatar: "/avatar/molihey.png",
+  avatar: "/avatar/color_profile.png",
   sources: ["https://qiita.com/Roy1473/feed.atom"],
   githubName: "https://github.com/Roy1473",
   twitterName: "https://twitter.com/roy1473",
