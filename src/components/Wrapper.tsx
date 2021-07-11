611493397370c96e3c15b12c028eb1398a7fb196
Roy1473 <pgajdmga@gmail.com>
2021-07-12 03:56:57 +0900

    rename: wrapper className n style

--- src/components/Wrapper.tsx
+++ src/components/Wrapper.tsx
@@ -1,7 +1,7 @@
 import styles from "@src/styles/components/Wrapper.module.scss";
 export const Wrapper: React.FC<{ children: React.ReactNode }> = (props) => {
-  return <div className={styles.wrapper}>{props.children}</div>;
+  return <div className={styles.default}>{props.children}</div>;
 };
 export const WideWrapper: React.FC<{ children: React.ReactNode }> = (props) => {
-  return <div className={styles.wide_wrapper}>{props.children}</div>;
+  return <div className={styles.wide}>{props.children}</div>;
 };
