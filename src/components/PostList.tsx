cc2f2706c2a345c5fce1a2911b61113350ff4479
Roy1473 <pgajdmga@gmail.com>
2021-07-15 14:19:12 +0900

    make 2 component files as one, PostLink to PostList.tsx

--- src/components/PostList.tsx
+++ src/components/PostList.tsx
@@ -1,10 +1,28 @@
 import { useState } from "react";
 import { PostItem } from "@src/types";
-import { PostLink } from "@src/components/PostLink";
+//import { PostLink } from "@src/components/PostLink";
+import dayjs from "dayjs";
 import styles from "@src/styles/components/PostList.module.scss";
 interface Props {
   items: PostItem[];
 }
+export const PostLink: React.FC<{ item: PostItem }> = (props) => {
+  const { title, contentSnippet, link, isoDate, dateMiliSeconds } = props.item;
+  return (
+    <article className={styles.post_link}>
+      <a className={styles.post_link_content} href={link}>
+        <img src="/icon/qiita.png" width="30" height="30" alt="qiita" />
+        <div className={styles.post_link_details}>
+          <h2 className={styles.post_link_title}>{title}</h2>
+          <time className={styles.post_link_at} dateTime={isoDate}>
+            {dayjs(isoDate).format("YYYY-MM-DD")}
+          </time>
+        </div>
+      </a>
+    </article>
+  );
+};
+
 export const PostList: React.FC<{ items: PostItem[] }> = (props) => {
   const [postItemCount, setPostItemCount] = useState<number>(32);
   const totalItemCount = props.items?.length || 0;
@@ -13,11 +31,11 @@ export const PostList: React.FC<{ items: PostItem[] }> = (props) => {
   }
   return (
     <>
-      <ul className={styles.post_list}>
+      <div className={styles.post_list}>
         {props.items.slice(0, postItemCount).map((item, i) => (
           <PostLink key={`post-item-${i}`} item={item} />
         ))}
-      </ul>
+      </div>
     </>
   );
 };
