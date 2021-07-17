e2d65ddb5901ac5a6425e14b6078f435854ab685
Roy1473 <pgajdmga@gmail.com>
2021-07-17 16:17:52 +0900

    add some components for tag list in article

--- src/components/tags/tagItems.tsx
+++ src/components/tags/tagItems.tsx
@@ -1,4 +1,5 @@
 import Link from "next/link";
+import twemoji from "twemoji";
 import styles from "@src/styles/pages/blog/BlogContent.module.scss";
 interface Props {
   id: string;
@@ -15,6 +16,31 @@ export const Tags: React.FC<{ tagLink: Props }> = (props) => {
         >
           {props.tagLink.name}
         </a>
+      </Link>
+    </>
+  );
+};
+export const TaggedList: React.FC<{ tags: Props[] }> = (props) => {
+  return (
+    <>
+      <div className={styles.taggedList_container}>
+        <div className={styles.taggedItems_container}>
+          <div dangerouslySetInnerHTML={{ __html: twemoji.parse("🏷") }} />
+          <div>tag</div>
+          {props.tags.map((tag, i) => (
+            <TaggedItems key={`tag-${i}`} tagItem={tag} />
+          ))}
+        </div>
+      </div>
+    </>
+  );
+};
+export const TaggedItems: React.FC<{ tagItem: Props }> = (props) => {
+  const name = props.tagItem.name;
+  return (
+    <>
+      <Link href={`/tags/${name.replace(/\./g, "").toLowerCase()}`}>
+        <a className={styles.tagName}>{name}</a>
       </Link>
     </>
   );
