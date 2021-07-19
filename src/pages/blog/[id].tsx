9573b1ec6d9f740d8e632b4aee4042bcdd668e39
Roy1473 <pgajdmga@gmail.com>
2021-07-19 20:15:10 +0900

    create: new ArticleDate component for looking easy in [id].tsx

--- src/pages/blog/[id].tsx
+++ src/pages/blog/[id].tsx
@@ -3,10 +3,13 @@ import { WideWrapper } from "@src/components/Wrapper";
 import { NextPage, GetStaticPaths, GetStaticProps } from "next";
 //blog config
 //import { config } from "@blog.config";
+import { PubDate, PreDate } from "@src/components/articles/ArticleDate";
 import { getBlogs, getPreview } from "@src/lib/blog";
 import { BlogLink } from "@src/components/BlogList";
 import { Tags, TaggedList } from "@src/components/tags/tagItems";
 import { SidebarProfile } from "@src/components/SidebarProfile";
+//twemoji
+import twemoji from "twemoji";
 //SEOコンポーネント
 import { BlogSEO } from "@src/components/BlogSEO";
 //OGP画像生成メソッド
@@ -40,8 +43,8 @@ interface TocList {
   name: string;
 }
 const Blog: NextPage<Props> = (props) => {
-  const { title, publishedAt, body, createdAt, updatedAt, tags, id } =
-    props.blog;
+  const { title, publishedAt, createdAt, updatedAt, tags, id } = props.blog;
+  const body = props.body;
   const category = props.category;
   //cloudinry で生成したOGPデータ
   const cl = props.cl;
@@ -68,6 +71,11 @@ const Blog: NextPage<Props> = (props) => {
                     alt={`${title}-image`}
                   />
                 </div>
+
+                <PreDate createdAt={createdAt} updatedAt={updatedAt} />
+
+                <PubDate publishedAt={publishedAt} />
+
                 {preview && (
                   <a
                     href="/api/clearPreview"
@@ -76,7 +84,6 @@ const Blog: NextPage<Props> = (props) => {
                     ** preview mode　解除 **
                   </a>
                 )}
-                <h1 className={styles.blog_content_title}>{title}</h1>
                 {preview ? (
                   <div className={styles.blog_content_article_at}>
                     <span className={styles.blog_content_article_at_list}>
@@ -92,14 +99,22 @@ const Blog: NextPage<Props> = (props) => {
                   </div>
                 ) : (
                   <div className={styles.blog_content_article_at}>
-                    <time>
-                      <span>
-                        公開日{dayjs(publishedAt).format("YYYY/MM/DD")}
-                      </span>
-                    </time>
+                    <span>
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="25"
+                        height="25"
+                        fill="currentColor"
+                        viewBox="0 0 16 16"
+                      >
+                        <path d="M8 3.5a.5.5 0 0 0-1 0V9a.5.5 0 0 0 .252.434l3.5 2a.5.5 0 0 0 .496-.868L8 8.71V3.5z" />
+                        <path d="M8 16A8 8 0 1 0 8 0a8 8 0 0 0 0 16zm7-8A7 7 0 1 1 1 8a7 7 0 0 1 14 0z" />
+                      </svg>
+                      <time>{dayjs(publishedAt).format("YYYY/MM/DD")}</time>
+                    </span>
                   </div>
                 )}
-                <div>{category}</div>
+                <h1 className={styles.blog_content_title}>{title}</h1>
                 <TaggedList tags={tags} />
                 <div
                   dangerouslySetInnerHTML={{
@@ -120,7 +135,6 @@ const Blog: NextPage<Props> = (props) => {
                   </div>
                 </div>
                 <div className={styles.blog_sidebar_sticky}>
-                  <SidebarProfile />
                   <div className={styles.blog_sidebar_toc}>
                     <div className={styles.blog_sidebar_toc_title}>目次</div>
                     <div className={styles.blog_sidebar_toc_area}>
