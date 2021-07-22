a8a7e948ce7a3a23230d39565ddef1d250f17fc5
Roy1473 <pgajdmga@gmail.com>
2021-07-22 21:26:38 +0900

    fix: svg fillRule

--- src/components/articles/ArticleDate.tsx
+++ src/components/articles/ArticleDate.tsx
@@ -18,7 +18,7 @@ export const PreDate: React.FC<PreviewDate> = (props) => {
   );
 };
 export const PubDate: React.FC<PublishDate> = (props) => {
-  const { publishedAt } = props;
+  const { publishedAt, updatedAt } = props;
   return (
     <>
       <div className={styles.container}>
@@ -34,6 +34,19 @@ export const PubDate: React.FC<PublishDate> = (props) => {
             <path d="M8 16A8 8 0 1 0 8 0a8 8 0 0 0 0 16zm7-8A7 7 0 1 1 1 8a7 7 0 0 1 14 0z" />
           </svg>
           <time>{dayjs(publishedAt).format("YYYY/MM/DD")}</time>
+        </span>
+        <span className={styles.up_date}>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="20"
+            height="20"
+            fill="currentColor"
+            viewBox="0 0 16 16"
+          >
+            <path d="M8 3a5 5 0 1 1-4.546 2.914.5.5 0 0 0-.908-.417A6 6 0 1 0 8 2v1z" />
+            <path d="M8 4.466V.534a.25.25 0 0 0-.41-.192L5.23 2.308a.25.25 0 0 0 0 .384l2.36 1.966A.25.25 0 0 0 8 4.466z" />
+          </svg>
+          <time>{dayjs(updatedAt).format("YYYY/MM/DD")}</time>
         </span>
       </div>
     </>
