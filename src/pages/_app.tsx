0ccb9b48582c5b75965e4d6a4309e8066c107828
Roy1473 <pgajdmga@gmail.com>
2021-08-19 18:37:39 +0900

    change: favicon

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -12,7 +12,7 @@ function MyApp({ Component, pageProps }: AppProps) {
           href="https://fonts.googleapis.com/css2?family=Inter:wght@200&family=Noto+Sans+JP:wght@100&display=swap"
           rel="stylesheet"
         />
-        <link rel="icon" href="avatar/molihey.png" />
+        <link rel="icon" href="avatar/color_profile.png" />
       </Head>
       <Header />
       <Component {...pageProps} />
