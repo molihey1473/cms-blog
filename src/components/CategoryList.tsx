17d58b67da7df5a231509647111c9ae85e16d84d
Roy1473 <pgajdmga@gmail.com>
2021-09-13 13:02:28 +0900

    rename: some components

--- src/components/CategoryList.tsx
+++ src/components/CategoryList.tsx
@@ -1,81 +1,25 @@
-import { useState, useCallback } from "react";
-import twemoji from "twemoji";
 import styles from "@src/styles/components/CategoryList.module.scss";
 import Link from "next/link";
 import { useRouter } from "next/router";
 interface Props {
   path: string;
-  title: string;
-  query?: string | undefined;
-  emoji?: string;
+  title: string | null;
 }
 
-export const CategoryItem: React.FC<Props> = (props) => {
-  const { path, query, title, emoji } = props;
-  const router = useRouter();
-  return (
-    <Link href={path}>
-      <a className={`${styles.navigation_item}`}>
-        <div
-          className={`${styles.category_list_emoji} ${
-            router.pathname === path ? styles.is_active : ""
-          }`}
-          dangerouslySetInnerHTML={{ __html: emoji }}
-        />
-        <div className={styles.link_name}>{title}</div>
-      </a>
-    </Link>
-  );
-};
 export const CategoryList: React.FC = () => {
-  return (
-    <nav className={styles.navigation_content}>
-      <CategoryItem path="/" title="All" emoji={twemoji.parse("📚")} />
-
-      <CategoryItem
-        path="/category/tech"
-        query="tech"
-        title="Tech"
-        emoji={twemoji.parse("🧬", {
-          folder: "svg",
-          ext: ".svg",
-        })}
-      />
-      <CategoryItem
-        path="/category/diary"
-        query="diary"
-        title="Diary"
-        emoji={twemoji.parse("🗞", {
-          folder: "svg",
-          ext: ".svg",
-        })}
-      />
-      <CategoryItem
-        path="/category/others"
-        title="Others"
-        emoji={twemoji.parse("🚀", {
-          folder: "svg",
-          ext: ".svg",
-        })}
-      />
-    </nav>
-  );
-};
-export const CategoryFlatList: React.FC = () => {
   return (
     <>
       <nav className={styles.navigation_content}>
-        <CategoryFlatItem path="/" title="All" />
-
-        <CategoryFlatItem path="/category/tech" query="tech" title="Tech" />
-        <CategoryFlatItem path="/category/diary" query="diary" title="Diary" />
-        <CategoryFlatItem path="/category/others" title="Others" />
+        <CategoryItems path="/" title="All" />
+        <CategoryItems path="/category/tech" title="Tech" />
+        <CategoryItems path="/category/diary" title="Diary" />
+        <CategoryItems path="/category/others" title="Others" />
       </nav>
     </>
   );
 };
-export const CategoryFlatItem: React.FC<Props> = (props) => {
-  const { path, query, title } = props;
+export const CategoryItems: React.FC<Props> = (props) => {
+  const { path, title } = props;
   const router = useRouter();
   return (
     <>
