beabf9e86acc3c9740d4d8625169f1ed5da32877
Roy1473 <pgajdmga@gmail.com>
2021-10-07 23:48:11 +0900

    fix: profile情報props修正

--- src/components/cards/Profile.tsx
+++ src/components/cards/Profile.tsx
@@ -1,12 +1,11 @@
 //import { member } from "@src/utils/member";
-import Image from "next/image";
 import { Member } from "@src/types";
 import { TwitterIcon } from "@src/components/icons/TwitterIcon";
 import { GithubIcon } from "@src/components/icons/GithubIcon";
 import styles from "@src/styles/components/profile.module.scss";
 
 export const Profile: React.FC<{ member: Member }> = (props) => {
-  const { avatar, name, bio, twitterName } = props.member;
+  const { avatar, name, bio, twitterName, githubName } = props.member;
   return (
     <div className={styles.profile_layout}>
       <div className={styles.profile_content}>
@@ -15,21 +14,30 @@ export const Profile: React.FC<{ member: Member }> = (props) => {
           <h1 className={styles.profile_member_name}>{name}</h1>
           <p className={styles.profile_member_bio}>{bio}</p>
           <div className={styles.profile_links}>
-            <a
-              className={styles.profile_link_item}
-              href="https://github.com/Roy1473"
-            >
+            <a className={styles.profile_link_item} href={`${githubName}`}>
               <GithubIcon />
             </a>
-            <a
-              className={styles.profile_link_item}
-              href={`https://twitter.com/${twitterName}`}
-            >
+            <a className={styles.profile_link_item} href={`${twitterName}`}>
               <TwitterIcon />
             </a>
           </div>
         </div>
       </div>
+    </div>
+  );
+};
+export const FooterProfile: React.FC<{ member: Member }> = (props) => {
+  const { avatar, name, bio, twitterName } = props.member;
+  return (
+    <div className={styles.md_profile_container}>
+      <div>
+        <img src="" alt="" />
+      </div>
+      <div>
+        <a href=""></a>
+        <a href=""></a>
+      </div>
+      footer
     </div>
   );
 };
