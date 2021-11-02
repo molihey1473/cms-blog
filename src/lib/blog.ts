49fc497cd9aa3c50deb01faabb9aa06d5a6a805b
Roy1473 <pgajdmga@gmail.com>
2021-11-02 23:17:54 +0900

    fix: 記事内用結合メソッド

--- src/lib/blog.ts
+++ src/lib/blog.ts
@@ -1,4 +1,5 @@
 import { BLOG_API, TAG_API, CATEGORY_API } from "@src/utils/blogInfo";
+import { isBodyString } from "@src/utils/isCodeString";
 import { ArticleItems, ArticleBodyItems } from "@src//types";
 import { highlight, languages } from "prismjs";
 import { ArticleBody } from "@src/components/articles/ArticleBody";
@@ -36,23 +37,53 @@ export const getPreview = async (
   const articleData = await fetch(`${BLOG_API}${id}${params}`, key)
     .then((res) => res.json())
     .catch((error) => null);
-  getCodeHighlight(articleData.body);
-  return articleData;
+  const body = fixArticle(articleData.body);
+  //getCodeHighlight(articleData.body);
+  //console.log(articleData.body);
+  return { ...articleData, ...body };
+  //return articleData;
 };
 
 //記事内ソースコートをハイライト処理
 export const getCodeHighlight = (body: ArticleBodyItems[]) => {
   for (const bodyItem of body) {
-    if (bodyItem.code !== null) {
+    if (!bodyItem?.code) {
+      bodyItem.code = null;
+      bodyItem.language = null;
+    } else {
       bodyItem.code = highlight(
         bodyItem.code,
         languages[bodyItem.language],
         bodyItem.language
       );
-    } else {
-      return body;
     }
   }
+};
+export const fixArticle = (
+  body: {
+    markdown: string;
+    language?: string;
+    code?: string;
+  }[]
+): { body: string } => {
+  for (const value of body) {
+    if (typeof value.code === "undefined") {
+      value.markdown = `<div className="blog_content_body">${value.markdown}</div>`;
+    } else {
+      value.markdown = `<div className="blog_content_body">${
+        value.markdown
+      }<div className="code-container">
+      <pre className=${`language-${value.language}`}>
+        <code className=${`language-${value.language}`}>${highlight(
+        value.code,
+        languages[value.language],
+        value.language
+      )}</code>
+      </pre>
+    </div></div>`;
+    }
+  }
+  return { body: body[0].markdown };
 };
 
 //get data for [name].tsx (getCategoryとほぼ同一メソッドなので修正検討中)
