6552600ffb277f2325561e13d4698ae6c0600223
Roy1473 <pgajdmga@gmail.com>
2021-11-10 09:16:00 +0900

    既存のコードを修正

--- src/pages/tags/[name].tsx
+++ src/pages/tags/[name].tsx
@@ -5,18 +5,19 @@ import { BlogLink } from "@src/components/BlogList";
 import twemoji from "twemoji";
 import { getTags } from "@src/lib/blog";
 //import { TagList } from "@src/components/tags/TagList";
-import { TaggedBlogs } from "@src/types";
+import { TaggedBlogs, Taglinks, TaggedList } from "@src/types";
+import { toStringName } from "@src/utils/toStringTagName";
 import styles from "@src/styles/pages/blog/BlogList.module.scss";
 interface Props {
   name: string;
-  content: {
+  taggedBlogs: {
     id: string;
     title: string;
     publishedAt: string;
-    meta?: { image: { url: string } };
   }[];
 }
-const Page: NextPage<{ taggedBlogs: Props }> = (props) => {
+const Page: NextPage<Props> = (props) => {
+  const { taggedBlogs, name } = props;
   return (
     <>
       <section className={styles.tagged_blog_list_layout}>
@@ -31,15 +32,17 @@ const Page: NextPage<{ taggedBlogs: Props }> = (props) => {
                   }),
                 }}
               />
-              <div
-                className={styles.tag_name}
-              >{`#${props.taggedBlogs.name}`}</div>
+              <div className={styles.tag_name}>{`#${name}`}</div>
             </div>
-            <div className={styles.tagged_blog_list}>
-              {props.taggedBlogs.content.map((taggedBlog, i) => (
-                <BlogLink key={`taggedBlog-${i}`} item={taggedBlog} />
-              ))}
-            </div>
+            {taggedBlogs ? (
+              <div className={styles.tagged_blog_list}>
+                {taggedBlogs.map((taggedBlog, i) => (
+                  <BlogLink key={`taggedBlog-${i}`} item={taggedBlog} />
+                ))}
+              </div>
+            ) : (
+              <div>関連した記事がありません</div>
+            )}
           </div>
         </Wrapper>
       </section>
@@ -47,30 +50,33 @@ const Page: NextPage<{ taggedBlogs: Props }> = (props) => {
   );
 };
 export const getStaticPaths: GetStaticPaths = async () => {
-  const data = await getTags();
+  const data = await getTags<Taglinks>();
   //paths [name]tsx意外
   const paths =
-    data.contents.map((content: { name: string; id: string }) => {
+    data.contents.map((items) => {
       //URL "/tags/[name]"の見た目をよくするため[name]を小文字、"."を消す。
       // [React, Vercel, Next.js]など
-      const lowScaleName = content.name.replace(/\./g, "").toLowerCase();
-      return `/tags/${lowScaleName}`;
+      const lowScaleName = items.name.replace(/\./g, "").toLowerCase();
+      return { params: { name: items.name } };
+      //return lowScaleName;
       //return `/tags/${content.name.replace(/\./, "").toLowerCase()}`;
     }) || [];
   return { paths, fallback: false };
 };
 export const getStaticProps: GetStaticProps = async ({ params }) => {
-  const name = params.name as string;
+  const name = toStringName(params.name);
   //tags CMSに関連した記事を抽出するため、再度、頭文字を大文字化
   const rename = name.charAt(0).toUpperCase();
   //idで抽出したデータではなく/tags/reactのようにnameで表示させる
-  const data = await getTags(rename);
+  const data = await getTags<TaggedList[]>(rename);
+  console.log(data);
   //const preData = data.contents[0].content.map((item, i) => {
   //  console.log(item.title, item.publishedAt);
   //});
   return {
     props: {
-      taggedBlogs: data.contents[0],
+      name: name,
+      taggedBlogs: data,
     },
   };
 };
