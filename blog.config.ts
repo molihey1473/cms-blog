05f489903fef69f7d2ed8351eed6c9cef4b05f0c
Roy1473 <pgajdmga@gmail.com>
2021-11-19 22:50:13 +0900

    vercel production domain 修正

--- blog.config.ts
+++ blog.config.ts
@@ -2,6 +2,6 @@ export const config = {
   defaultMeta: { title: "MoliHey", description: "MoliHeyのblog" },
   siteRoot:
     process.env.NODE_ENV === "production"
-      ? "https://blog-roy1473.vercel.app"
+      ? "https://molihey.vercel.app"
       : "http://localhost:3000",
 };
