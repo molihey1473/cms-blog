3da22a645d638aaafdcef751f3d4f88b022013ad
Roy1473 <pgajdmga@gmail.com>
2021-11-27 18:38:19 +0900

    fix ogp: summary summary_large 使い分け修正

--- src/components/BlogSEO.tsx
+++ src/components/BlogSEO.tsx
@@ -5,9 +5,10 @@ interface Props {
   path?: string;
   id?: string;
   image?: string;
+  largeImage: boolean;
 }
 export const BlogSEO: React.FC<Props> = (props) => {
-  const { title, path, image } = props;
+  const { title, path, image, largeImage } = props;
   const pageUrl = `${config.siteRoot}${path || ""}`;
   return (
     <Head>
@@ -15,11 +16,25 @@ export const BlogSEO: React.FC<Props> = (props) => {
       <meta property="og:title" content={title} />
       <meta property="og:type" content="article" />
       <meta property="og:url" content={pageUrl} />
-      <meta
-        property="og:image"
-        content={image || `${config.siteRoot}/ogp/home_ogp.png`}
-      />
+
       <meta property="og:site_name" content="MoliHey" />
+      {largeImage ? (
+        <>
+          <meta name="twitter:card" content="summary_large_image" />
+          <meta
+            property="og:image"
+            content={image || `${config.siteRoot}/ogp/home_ogp.png`}
+          />
+        </>
+      ) : (
+        <>
+          <meta name="twitter:card" content="summary" />
+          <meta
+            property="og:image"
+            content={`${config.siteRoot}/avatar/color_profile.png`}
+          />
+        </>
+      )}
       <meta name="twitter:card" content="summary_large_image" />
       {path && <link ref="canonical" href={pageUrl} />}
     </Head>
