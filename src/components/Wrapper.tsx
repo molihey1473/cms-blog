b3287bd8fce009a8a598e53e2807194c4bdc0028
Roy1473 <pgajdmga@gmail.com>
2022-01-08 23:37:04 +0900

    確認用Wrapper component, インラインcss

--- src/components/Wrapper.tsx
+++ src/components/Wrapper.tsx
@@ -1,7 +1,10 @@
 import styles from "@src/styles/components/Wrapper.module.scss";
+import { ReactNode } from "react";
+//ページ共通Warpper
 export const Wrapper: React.FC<{ children: React.ReactNode }> = (props) => {
   return <div className={styles.default}>{props.children}</div>;
 };
+//共通よりもwidthが広いWrapper
 export const WideWrapper: React.FC<{ children: React.ReactNode }> = (props) => {
   return <div className={styles.wide}>{props.children}</div>;
 };
@@ -9,4 +12,16 @@ export const ArticleWrapper: React.FC<{ children: React.ReactNode }> = (
   props
 ) => {
   return <div className={styles.article}>{props.children}</div>;
+};
+
+//min-heightを含んだmain view用wrapper
+export const MainViewWrapper: React.FC<{ children: ReactNode }> = (props) => {
+  const widthstyle: { [key: string]: string } = {
+    "min-height": "calc(100vh - 60px)",
+  };
+  return (
+    <div style={widthstyle}>
+      <div className={styles.default}>{props.children}</div>;
+    </div>
+  );
 };
