f2374a80750e9dfde186ca8a83ebfce3ae7e7ab9
Roy1473 <pgajdmga@gmail.com>
2022-01-14 23:44:35 +0900

    コメントアウト

--- src/components/View.tsx
+++ src/components/View.tsx
@@ -1,6 +1,10 @@
 import { ReactNode } from "react";
 import styles from "@src/styles/components/View.module.scss";
-//param children 全ページ共通 Viewコンポーネント
+/*各ページのレイアウト
+ 現在、記事ページ、ランディングページ等全て共通Wrapper component(同じ画面幅)
+*/
+
+// 全ページ共通 Viewコンポーネント
 export const ViewContainer: React.FC<{ children: ReactNode }> = (props) => {
   return <div className={styles.main_container}>{props.children}</div>;
 };
