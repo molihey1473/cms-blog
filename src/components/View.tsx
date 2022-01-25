d3f48fb814f6b22d72c8308320d8073945cdb8e4
Roy1473 <pgajdmga@gmail.com>
2022-01-25 23:35:43 +0900

    add new View component: 記事一覧用コンポーネント

--- src/components/View.tsx
+++ src/components/View.tsx
@@ -1,4 +1,4 @@
-import { ReactNode } from "react";
+import React, { ReactNode } from "react";
 import styles from "@src/styles/components/View.module.scss";
 /*各ページのレイアウト
  現在、記事ページ、ランディングページ等全て共通Wrapper component(同じ画面幅)
@@ -6,5 +6,12 @@ import styles from "@src/styles/components/View.module.scss";
 
 // 全ページ共通 Viewコンポーネント
 export const ViewContainer: React.FC<{ children: ReactNode }> = (props) => {
+  return <div className={styles.main_container}>{props.children}</div>;
+};
+
+// 記事一覧用View　コンポーネント
+export const ViewMainContainer: React.FC<{ children: React.ReactNode }> = (
+  props
+) => {
   return <div className={styles.main_container}>{props.children}</div>;
 };
