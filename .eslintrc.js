fbb7080b5cdff3e35d9d7d930771ea59807265cd
Roy1473 <pgajdmga@gmail.com>
2022-03-03 23:56:36 +0900

    fix eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,22 +4,89 @@ module.exports = {
     es2021: true,
     node: true,
   },
-  parser: `@typescript-eslint/parser`,
-  plugins: [`@typescript-eslint`],
+  parser: "@typescript-eslint/parser",
+  plugins: ["@typescript-eslint", "react", "import"],
   parserOptions: {
+    tsconfigRootDir: __dirname,
     project: ["./tsconfig.json"],
     sourceType: "module",
     ecmaFeatures: {
       jsx: true,
     },
   },
-  rules: {
-    semi: "off",
+  settings: {
+    react: {
+      version: "detect",
+    },
+    "import/resolver": {
+      typescript: {
+        alwaysTryTypes: true,
+      },
+    },
   },
   extends: [
     "eslint:recommended",
-    "plugin:@typescript-eslint/eslint-recommended",
+    "plugin:react/recommended",
+    "plugin:import/errors",
+    "plugin:import/warnings",
+    "plugin:import/typescript",
     "plugin:@typescript-eslint/recommended",
+    "plugin:@next/next/recommended",
     "prettier",
   ],
+  rules: {
+    semi: "off",
+    "react/prop-types": "off",
+    "react/react-in-jsx-scope": "off",
+    "@next/next/no-img-element": "off",
+    "@next/next/no-html-link-for-pages": "off",
+    "import/order": [
+      "error",
+      {
+        groups: [
+          "index",
+          "sibling",
+          "parent",
+          "internal",
+          "external",
+          "builtin",
+          "object",
+          "type",
+        ],
+        "newlines-between": "always",
+        pathGroupsExcludedImportTypes: ["builtin"],
+        pathGroups: [
+          {
+            pattern: "@src/components/**",
+            group: "internal",
+            position: "before",
+          },
+          {
+            pattern: "@src/lib/**",
+            group: "internal",
+            position: "before",
+          },
+          {
+            pattern: "@src/styles/components/*.module.scss",
+            group: "internal",
+            position: "after",
+          },
+          {
+            pattern: "@src/types.ts",
+            group: "internal",
+            position: "after",
+          },
+          {
+            pattern: "@src/utils/**",
+            group: "internal",
+            position: "before",
+          },
+        ],
+        alphabetize: {
+          order: "asc",
+          caseInsensitive: true,
+        },
+      },
+    ],
+  },
 };
