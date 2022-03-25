7b53497dc0e474f0dd040a206705b62763e1e084
Roy1473 <pgajdmga@gmail.com>
2022-03-25 21:26:31 +0900

    fix import

--- src/lib/blog.ts
+++ src/lib/blog.ts
@@ -1,13 +1,9 @@
-import { BLOG_API, TAG_API, CATEGORY_API } from "@src/utils/blogInfo";
-import {
-  ArticleItems,
-  ArticleBodyItems,
-  TaggedList,
-  TagItems,
-  TaggedBlogs,
-} from "@src//types";
 import { highlight, languages } from "prismjs";
-import { ArticleBody } from "@src/components/articles/ArticleBody";
+
+import { ArticleItems, ArticleBodyItems } from "@src/types/types";
+
+import { BLOG_API, TAG_API, CATEGORY_API } from "@src/utils/blogInfo";
+
 // microCMS API KEY
 const key = {
   headers: {
@@ -41,7 +37,7 @@ export const getPreview = async (
   const params = draftKey ? `?draftKey=${draftKey}` : "";
   const articleData = await fetch(`${BLOG_API}${id}${params}`, key)
     .then((res) => res.json())
-    .catch((error) => null);
+    .catch((error) => console.error("エラーが発生", error));
   const body = fixArticle(articleData.body);
   //getCodeHighlight(articleData.body);
   //console.log(articleData.body);
@@ -98,12 +94,12 @@ export const getTags = async <T>(name?: string): Promise<T> => {
     const url = `${TAG_API}${nameSlug}`;
     const preData = await fetch(url, key)
       .then((res) => res.json())
-      .catch((error) => null);
+      .catch((error) => console.error("エラーが発生", error));
     return preData.contents[0]?.content;
   } else {
     return await fetch(TAG_API, key)
       .then((res) => res.json())
-      .catch((error) => null);
+      .catch((error) => console.error("エラーが発生", error));
   }
 };
 // get data for /category/[name].tsx (getTagとほぼ同一メソッドなので修正検討中)
@@ -111,8 +107,8 @@ export const getCategory = async (): Promise<string[]> => {
   const path = CATEGORY_API;
   const data = await fetch(path, key)
     .then((res) => res.json())
-    .catch((error) => null);
-  return data.contents.map((item, i) => {
+    .catch((error) => console.error("エラーが発生", error));
+  return data.contents.map((item: { name: string[] }) => {
     return `/category/${item.name[0]}`;
   });
 };
