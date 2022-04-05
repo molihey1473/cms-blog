feda4b615be8d9999068303699d3e065e0ed0018
Roy1473 <pgajdmga@gmail.com>
2022-04-05 23:41:19 +0900

    propsに型挿入（仮）

--- src/components/icons/Twemoji.tsx
+++ src/components/icons/Twemoji.tsx
@@ -1,3 +1,15 @@
-export const Twemoji = (props) => {
-  return <img src={props.emoji} />;
+import { parse } from "twemoji-parser";
+
+import React from "react";
+
+export const TwemojiP: React.FC<{ emoji: { url: string }[] }> = (props) => {
+  const { emoji } = props;
+  const parseEmoji = parse(emoji);
+
+  console.log(parseEmoji);
+  return (
+    <>
+      <img src={emoji[0].url} />
+    </>
+  );
 };
