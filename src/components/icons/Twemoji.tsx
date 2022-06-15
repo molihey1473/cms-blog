e33cb12a78717015d444aa00245c8c897a74b18c
Roy1473 <pgajdmga@gmail.com>
2022-06-16 06:13:39 +0900

    fix

--- src/components/icons/Twemoji.tsx
+++ src/components/icons/Twemoji.tsx
@@ -5,8 +5,6 @@ import React from "react";
 export const TwemojiP: React.FC<{ emoji: string }> = (props) => {
   const { emoji } = props;
   const parseEmoji = parse(emoji);
-
-  console.log(parseEmoji);
   return (
     <>
       <img src={parseEmoji[0]?.url} />
