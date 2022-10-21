d4e5cbb122c6b7ba02966eceba7309c8321a5015
Roy1473 <pgajdmga@gmail.com>
2022-10-21 22:22:15 +0900

    twemoji component削除

--- src/components/TagTitle.tsx
+++ src/components/TagTitle.tsx
@@ -1,14 +1,9 @@
-import { Twemoji } from "@src/components/icons/Twemoji";
-
 import styles from "@src/styles/pages/blog/BlogList.module.scss";
 
 export const TagTitle: React.FC<{ title: string }> = (props) => {
   return (
     <div className={styles.tag_name_container}>
-      <h1 className={styles.tag_name}>
-        <Twemoji emoji={"✊"} />
-        {`#${props.title}`}
-      </h1>
+      <h1 className={styles.tag_name}>{`#${props.title}`}</h1>
     </div>
   );
 };
