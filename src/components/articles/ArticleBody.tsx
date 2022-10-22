7d0ce628aa3a6687802c514a13c216b5f42a156e
Roy1473 <pgajdmga@gmail.com>
2022-10-22 23:58:30 +0900

    remove: comment

--- src/components/articles/ArticleBody.tsx
+++ src/components/articles/ArticleBody.tsx
@@ -1,33 +1,4 @@
 import parse from "html-react-parser";
-//import { highlight, languages } from "prismjs";
-//
-//import { ArticleBodyItems } from "@src/types/types";
-
-//export const ArticleBody: React.FC<{ articleBody: ArticleBodyItems[] }> = (
-//  props
-//) => {
-//  const { articleBody } = props;
-//  return (
-//    <>
-//      {articleBody.map((item, i) => {
-//        const { markdown, language, code } = item;
-//        return (
-//          <div key={`body-content-${i}`} className="blog_content_body">
-//            {parse(markdown)}
-//            {code && (
-//              <div className="code-container">
-//                <pre className={`language-${language}`}>
-//                  <code className={`language-${language}`}>{parse(code)}</code>
-//                </pre>
-//              </div>
-//            )}
-//          </div>
-//        );
-//      })}
-//    </>
-//  );
-//};
-
 export const FixArticleBody: React.FC<{ articleBody: string }> = (props) => {
   const { articleBody } = props;
   return (
