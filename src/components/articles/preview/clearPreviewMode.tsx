871d355aff546e32d18468b1a9e2f1f969fbacb5
Roy1473 <pgajdmga@gmail.com>
2022-10-24 23:32:08 +0900

    fix: component name

--- src/components/articles/preview/clearPreviewMode.tsx
+++ src/components/articles/preview/clearPreviewMode.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 import styles from "@src/styles/pages/blog/BlogContent.module.scss";
 
-export const clearPreviewMode: React.FC<{ preview: boolean }> = () => {
+export const ClearPreviewMode: React.FC = () => {
   return (
     <Link href={"/api/clearPreview"}>
       <a className={styles.clear_preview_mode}>** preview mode 解除 **</a>
