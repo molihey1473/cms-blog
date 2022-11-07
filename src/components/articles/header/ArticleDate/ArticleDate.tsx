b86edf75bad151de64561b43e31b6fb9ff8784a1
Roy1473 <pgajdmga@gmail.com>
2022-11-07 21:24:52 +0900

    fix: PubDate PreDate 共通化コンポーネント作成

--- src/components/articles/header/ArticleDate/ArticleDate.tsx
+++ src/components/articles/header/ArticleDate/ArticleDate.tsx
@@ -2,7 +2,12 @@ import dayjs from "dayjs";
 
 import { UpdateIcon, PublishDateIcon } from "@src/components/icons";
 
-import { PreviewDate, PublishDate } from "@src/types/types";
+import {
+  PreviewDate,
+  PublishDate,
+  ArticleDate,
+  ArticleDate,
+} from "@src/types/types";
 
 import styles from "@src/styles/components/articles/header/ArticleHeader.module.scss";
 
@@ -37,6 +42,36 @@ export const PubDate: React.FC<PublishDate> = (props) => {
           </span>
         )}
       </div>
+    </>
+  );
+};
+export const ArticleDate: React.FC<ArticleDate> = (props) => {
+  const { createdAt, updatedAt, publishedAt, preview } = props;
+  return (
+    <>
+      {preview ? (
+        <div className={styles.date_container}>
+          <span className={styles.create_date}>
+            <time>作成日：{dayjs(createdAt).format("YYYY/MM/DD")}</time>
+          </span>
+          <span className={styles.up_date}>
+            <time>更新日:{dayjs(updatedAt).format("YYYY/MM/DD")}</time>
+          </span>
+        </div>
+      ) : (
+        <div className={styles.date_container}>
+          <span className={styles.pub_date}>
+            <PublishDateIcon />
+            <time>{dayjs(publishedAt).format("YYYY/MM/DD")}</time>
+          </span>
+          {updatedAt && (
+            <span className={styles.up_date}>
+              <UpdateIcon />
+              <time>{dayjs(updatedAt).format("YYYY/MM/DD")}更新</time>
+            </span>
+          )}
+        </div>
+      )}
     </>
   );
 };
