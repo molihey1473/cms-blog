da8077c3632b0a4f558662391d0c0cd956477735
Roy1473 <pgajdmga@gmail.com>
2022-11-08 22:47:36 +0900

    fix: コンポーネント削除

--- src/components/articles/header/ArticleDate/ArticleDate.tsx
+++ src/components/articles/header/ArticleDate/ArticleDate.tsx
@@ -2,44 +2,10 @@ import dayjs from "dayjs";
 
 import { UpdateIcon, PublishDateIcon } from "@src/components/icons";
 
-import { PreviewDate, PublishDate, ArticleDate } from "@src/types/types";
+import { ArticleDate } from "@src/types/types";
 
 import styles from "@src/styles/components/articles/header/ArticleHeader.module.scss";
 
-export const PreDate: React.FC<PreviewDate> = (props) => {
-  const { createdAt, updatedAt } = props;
-  return (
-    <>
-      <div className={styles.date_container}>
-        <span className={styles.create_date}>
-          <time>作成日：{dayjs(createdAt).format("YYYY/MM/DD")}</time>
-        </span>
-        <span className={styles.up_date}>
-          <time>更新日:{dayjs(updatedAt).format("YYYY/MM/DD")}</time>
-        </span>
-      </div>
-    </>
-  );
-};
-export const PubDate: React.FC<PublishDate> = (props) => {
-  const { publishedAt, updatedAt } = props;
-  return (
-    <>
-      <div className={styles.date_container}>
-        <span className={styles.pub_date}>
-          <PublishDateIcon />
-          <time>{dayjs(publishedAt).format("YYYY/MM/DD")}</time>
-        </span>
-        {updatedAt && (
-          <span className={styles.up_date}>
-            <UpdateIcon />
-            <time>{dayjs(updatedAt).format("YYYY/MM/DD")}更新</time>
-          </span>
-        )}
-      </div>
-    </>
-  );
-};
 export const Date: React.FC<ArticleDate> = (props) => {
   const { createdAt, updatedAt, publishedAt, preview } = props;
   return (
