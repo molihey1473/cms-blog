e75bd71a8de93874586cc1a177985f089362b589
Roy1473 <pgajdmga@gmail.com>
2022-11-29 15:24:12 +0900

    rename: component name and file path

--- src/components/articles/layouts/main/ArticleMainView.tsx
+++ src/components/articles/layouts/main/ArticleMainView.tsx
@@ -1,5 +1,7 @@
 import { Wrapper } from "@src/components/Wrapper";
-export const ArticleMain: React.FC<{ children: React.ReactNode }> = (props) => {
+export const ArticleMainView: React.FC<{ children: React.ReactNode }> = (
+  props
+) => {
   return (
     <>
       <Wrapper> {props.children}</Wrapper>
