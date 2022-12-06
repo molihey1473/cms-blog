85fcc28ca6d2e2fdfd2e9270509d68ca6f34636e
Roy1473 <pgajdmga@gmail.com>
2022-12-06 23:45:18 +0900

    fix: remove Wrapper component

--- src/layouts/ArticleHeaderView/ArticleHeaderView.tsx
+++ src/layouts/ArticleHeaderView/ArticleHeaderView.tsx
@@ -1,12 +1,9 @@
-import { Wrapper } from "@src/components/Wrapper";
 export const ArticleHeaderView: React.FC<{ children: React.ReactNode }> = (
   props
 ) => {
   return (
     <>
-      <header>
-        <Wrapper>{props.children}</Wrapper>
-      </header>
+      <header>{props.children}</header>
     </>
   );
 };
