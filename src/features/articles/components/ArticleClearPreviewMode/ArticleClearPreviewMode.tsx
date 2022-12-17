d893a98d65e0c211aee174671edbdaa8e1c1c8c2
Roy1473 <pgajdmga@gmail.com>
2022-12-18 06:06:59 +0900

    fix: import style path

--- src/features/articles/components/ArticleClearPreviewMode/ArticleClearPreviewMode.tsx
+++ src/features/articles/components/ArticleClearPreviewMode/ArticleClearPreviewMode.tsx
@@ -1,6 +1,6 @@
 import Link from "next/link";
 
-import styles from "@src/styles/pages/blog/BlogContent.module.scss";
+import styles from "./ArticleClearPreviewMode.module.scss";
 interface Props {
   preview: boolean;
 }
