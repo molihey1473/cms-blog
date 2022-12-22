651e73fcd280e0c5a7863bd51760ec9b6cedf447
Roy1473 <pgajdmga@gmail.com>
2022-12-22 19:16:23 +0900

    fix: type as const

--- src/utils/member.ts
+++ src/utils/member.ts
@@ -1,10 +1,9 @@
-import { Member } from "@src/types/types";
 //blog user information
-export const member: Member = {
+export const member = {
   name: "Molihey",
   avatar: "/avatar/molihey_profile.png",
   sources: ["https://qiita.com/Roy1473/feed.atom"],
   githubName: "https://github.com/Roy1473",
   twitterName: "https://twitter.com/roy1473",
   bio: "React Next.jsにハマってます。",
-};
+} as const;
