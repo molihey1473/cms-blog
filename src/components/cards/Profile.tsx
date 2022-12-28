6c5a725f9e38b6bb7f2410e1b1fff7fdeb67f5aa
Roy1473 <pgajdmga@gmail.com>
2022-12-28 16:26:16 +0900

    mv: AsideProfile component

--- src/components/cards/Profile.tsx
+++ src/components/cards/Profile.tsx
@@ -35,26 +35,3 @@ export const Profile: React.FC<{ member: Member }> = (props) => {
     </div>
   );
 };
-export const AsideProfile: React.FC<{ member: Member }> = (props) => {
-  const { avatar, name, bio, twitterName, githubName } = props.member;
-  return (
-    <div className={styles.md_container}>
-      <div className={styles.md_image}>
-        <Image src={`${avatar}`} width={80} height={80} alt={`${name}`} />
-      </div>
-
-      <div className={styles.md_content}>
-        <div className={styles.md_author_name}>{name}</div>
-        <p className={styles.md_bio}>{bio}</p>
-        <div className={styles.md_bio_link_content}>
-          <a className={styles.md_bio_link} href={`${githubName}`}>
-            <GithubIcon />
-          </a>
-          <a className={styles.md_bio_link} href={`${twitterName}`}>
-            <TwitterIcon />
-          </a>
-        </div>
-      </div>
-    </div>
-  );
-};
