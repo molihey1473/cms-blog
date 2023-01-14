80c388aaf5b7aaa651f7db4add300bf53e50f9e5
Roy1473 <pgajdmga@gmail.com>
2023-01-15 08:08:54 +0900

    fix: import path

--- src/features/Top/components/PageTitle/PageTitle.tsx
+++ src/features/Top/components/PageTitle/PageTitle.tsx
@@ -1,4 +1,4 @@
-import styles from "@src/styles/components/View.module.scss";
+import styles from "./PageTitle.module.scss";
 export const PageTitle: React.FC<{ title: string }> = (props) => {
   return <h2 className={styles.page_content_title}>{props.title}</h2>;
 };
