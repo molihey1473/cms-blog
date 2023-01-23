db21dca22268e8674b3ee0af4992464f2d729916
Roy1473 <pgajdmga@gmail.com>
2023-01-23 18:40:49 +0900

    fix: component name

--- src/layouts/PageWrapper/PageWrapper.tsx
+++ src/layouts/PageWrapper/PageWrapper.tsx
@@ -1,6 +1,6 @@
 import styles from "./Wrapper.module.scss";
 
 //ページ共通Warpper
-export const Wrapper: React.FC<{ children: React.ReactNode }> = (props) => {
+export const PageWrapper: React.FC<{ children: React.ReactNode }> = (props) => {
   return <div className={styles.default}>{props.children}</div>;
 };
