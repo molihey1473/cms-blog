ec93441504d518d233938def72cfe29ecc336fe1
Roy1473 <pgajdmga@gmail.com>
2023-01-24 14:15:42 +0900

    fix: remove import component

--- src/features/Article/layouts/ArticleContainerView/ArticleContainerView.tsx
+++ src/features/Article/layouts/ArticleContainerView/ArticleContainerView.tsx
@@ -1,14 +1,10 @@
-import { Wrapper } from "@src/components/Wrapper";
-
 import styles from "./ArticleContainerView.module.scss";
 export const ArticleContainerView: React.FC<{ children: React.ReactNode }> = ({
   children,
 }) => {
   return (
     <>
-      <article className={styles.article_container}>
-        <Wrapper>{children}</Wrapper>
-      </article>
+      <article className={styles.article_container}>{children}</article>
     </>
   );
 };
