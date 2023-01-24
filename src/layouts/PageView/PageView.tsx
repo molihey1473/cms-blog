9955c6bcfa2655de4a7d6410f0ed20ced5b1f79a
Roy1473 <pgajdmga@gmail.com>
2023-01-24 11:02:54 +0900

    fix: component name

--- src/layouts/PageView/PageView.tsx
+++ src/layouts/PageView/PageView.tsx
@@ -3,13 +3,6 @@ import React, { ReactNode } from "react";
 import styles from "@src/styles/components/View.module.scss";
 
 // 全ページ共通 Viewコンポーネント
-export const ViewContainer: React.FC<{ children: ReactNode }> = (props) => {
-  return <div className={styles.main_container}>{props.children}</div>;
-};
-
-// 記事一覧用View コンポーネント
-export const ViewMainContainer: React.FC<{ children: React.ReactNode }> = (
-  props
-) => {
+export const PageView: React.FC<{ children: ReactNode }> = (props) => {
   return <div className={styles.main_container}>{props.children}</div>;
 };
