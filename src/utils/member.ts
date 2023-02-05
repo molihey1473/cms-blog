545eb81d48e72986e3da3a82bd22b02d97a3384c
Roy1473 <pgajdmga@gmail.com>
2023-02-06 07:33:04 +0900

    fix:sources url

--- src/utils/member.ts
+++ src/utils/member.ts
@@ -2,7 +2,7 @@
 export const member = {
   name: "Molihey",
   avatar: "/avatar/molihey_profile.png",
-  sources: ["https://qiita.com/Roy1473/feed.atom"],
+  sources: ["https://qiita.com/MoliHey/feed"],
   githubName: "https://github.com/Roy1473",
   twitterName: "https://twitter.com/roy1473",
   bio: "React Next.jsにハマってます。",
