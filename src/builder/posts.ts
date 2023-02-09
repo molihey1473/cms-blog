ad8c0982659613eb5763c708e4b8556f18abec3f
Roy1473 <pgajdmga@gmail.com>
2023-02-09 22:55:15 +0900

    get json from RSS and API cmsData

--- src/builder/posts.ts
+++ src/builder/posts.ts
@@ -1,14 +1,24 @@
 import { ensureDirSync, writeJsonSync } from "fs-extra";
+import fetch from "node-fetch";
 import Parser from "rss-parser";
 
 import { FeedItem } from "@src/types/types";
 
+import { BLOG_API } from "@src/utils/blogInfo";
 import { member } from "@src/utils/member";
+
+import { scriptENV } from "@.env.config";
 
 //  # import { member } from "@src/utils/member" だとError: Cannot find module '@src/utils/member'になる
 //export default {};
+interface fetchData {
+  title: string;
+  id: string;
+  publishedAt: string;
+}
+
 const parser = new Parser();
-async function fetchFeedItems(url: string) {
+export async function fetchFeedItems(url: string) {
   const feed = await parser.parseURL(url);
   if (!feed?.items?.length) return [];
   return feed.items
@@ -18,23 +28,48 @@ async function fetchFeedItems(url: string) {
         title,
         contentSnippet: contentSnippet?.replace(/\n/g, ""),
         link,
-        isoDate,
+        date: isoDate,
         dateMiliSeconds: isoDate ? new Date(isoDate).getTime() : 0,
       };
     })
     .filter(({ title, link }) => title && link) as FeedItem[];
 }
-async function getFeedItemsFromSources(sources: undefined | readonly string[]) {
+export async function getFeedItemsFromSources(
+  sources: undefined | readonly string[]
+) {
   if (!sources?.length) return [];
   let feedItems: FeedItem[] = [];
+  const data = await fetchArticleDataFromMicroCMS();
   for (const url of sources) {
     const items = await fetchFeedItems(url);
     if (items) feedItems = [...feedItems, ...items];
   }
-  return feedItems;
+  return [...feedItems, ...data];
+}
+export async function fetchArticleDataFromMicroCMS(): Promise<FeedItem[]> {
+  const data = await fetchFromMicroCMS();
+  if (!data?.length) return [];
+  return data.map(({ title, id, publishedAt }) => {
+    return {
+      category: "MicroCMSArticle",
+      title,
+      contentSnippet: null,
+      link: `/articles/${id}`,
+      date: publishedAt,
+      dateMiliSeconds: publishedAt ? new Date(publishedAt).getTime() : 0,
+    };
+  });
+}
+export async function fetchFromMicroCMS(): Promise<fetchData[]> {
+  const data = await fetch(BLOG_API, scriptENV)
+    .then((res) => res.json())
+    .catch((error) => console.log("通信失敗", error));
+  console.log(data);
+  const resultData = data.contents as fetchData[];
+  return resultData || [];
 }
 (async function () {
-  const items = (await getFeedItemsFromSources(member.sources)) ?? [];
+  const items = await getFeedItemsFromSources(member.sources);
   items.sort((a, b) => b.dateMiliSeconds - a.dateMiliSeconds);
   ensureDirSync(".contents");
   writeJsonSync(".contents/posts.json", items);
