1b7920ee16debaa7e15a52e4de3b90f51128ba2d
Roy1473 <pgajdmga@gmail.com>
2023-02-16 04:40:06 +0900

    fix: replace new components

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,37 +1,49 @@
 import { GetStaticProps, NextPage } from "next";
 
-import { BlogFlatList, BlogList } from "@src/components/BlogList";
-import { BlogSEO } from "@src/components/BlogSEO";
-import { Profile } from "@src/components/cards/Profile";
-import { CategoryList } from "@src/components/CategoryList";
-import { PageTitle } from "@src/components/PageTitle";
+//import { BlogFlatList, BlogList } from "@src/components/BlogList";
+//import { Profile } from "@src/components/cards/Profile";
+//import { CategoryList } from "@src/components/CategoryList";
+import { PageSEO } from "@src/components/PageSEO";
+//import { PageTitle } from "@src/components/PageTitle";
 
 import { getAllArticles } from "@src/lib/blog";
 
 import { ArticleItems } from "@src/types/types";
 
-import { member } from "@src/utils/member";
+import { Top } from "@src/features/Top";
+import { PageView } from "@src/layouts/PageView";
+import { PageWrapper } from "@src/layouts/PageWrapper";
+//import { member } from "@src/utils/member";
 
-import styles from "@src/styles/components/View.module.scss";
+//import styles from "@src/styles/components/View.module.scss";
 
-const Home: NextPage<{ allArticlesData: ArticleItems[]; category: string }> = (
+const Home: NextPage<{ articleListData: ArticleItems[]; category: string }> = (
   props
 ) => {
+  const { articleListData } = props;
   return (
+    // <>
+    //   <PageSEO title={"MoliHey"} isSummaryLarge={true} />
+    //   <Profile member={member} />{ }
+    //   <CategoryList />
+    //   <div className={styles.page_all_container}>
+    //     <PageTitle title={"ALL"} />
+    //     <div className={styles.all_items}>
+    //       {props.allArticlesData ? (
+    //         <BlogFlatList items={props.allArticlesData} />
+    //       ) : (
+    //         <BlogList items={props.allArticlesData} />
+    //       )}
+    //     </div>
+    //   </div>
+    // </>
     <>
-      <BlogSEO title={"MoliHey"} isSummaryLarge={true} />
-      <Profile member={member} />
-      <CategoryList />
-      <div className={styles.page_all_container}>
-        <PageTitle title={"ALL"} />
-        <div className={styles.all_items}>
-          {props.allArticlesData ? (
-            <BlogFlatList items={props.allArticlesData} />
-          ) : (
-            <BlogList items={props.allArticlesData} />
-          )}
-        </div>
-      </div>
+      <PageSEO title={"MoliHey"} isSummaryLarge={true} />
+      <PageView>
+        <PageWrapper>
+          <Top articleListData={articleListData} pageWithCategory={true} />
+        </PageWrapper>
+      </PageView>
     </>
   );
 };
@@ -39,7 +51,7 @@ export const getStaticProps: GetStaticProps = async () => {
   const data: { contents: ArticleItems[] } = await getAllArticles();
   return {
     props: {
-      allArticlesData: data.contents,
+      articleListData: data.contents,
     },
   };
 };
