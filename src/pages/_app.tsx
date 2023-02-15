956164403c2105f459b384e7432e986715574490
Roy1473 <pgajdmga@gmail.com>
2023-02-15 16:24:02 +0900

    fix

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,29 +1,18 @@
 import { AppProps } from "next/app";
 import Head from "next/head";
 
-import { Footer } from "@src/components/Footer";
-import { Header } from "@src/components/Header";
-import { ViewContainer } from "@src/components/View";
-import { Wrapper } from "@src/components/Wrapper";
-
+import { Footer } from "@src/layouts/Footer";
+import { Header } from "@src/layouts/Header";
 import "@src/styles/globals/index.scss";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
-        {/*<link
-          href="https://fonts.googleapis.com/css2?family=Inter:wght@200&family=Noto+Sans+JP:wght@100&display=swap"
-          rel="stylesheet"
-        /> */}
         <link rel="icon" href="avatar/color_profile.png" />
       </Head>
       <Header />
-      <ViewContainer>
-        <Wrapper>
-          <Component {...pageProps} />
-        </Wrapper>
-      </ViewContainer>
+      <Component {...pageProps} />
       <Footer />
     </>
   );
