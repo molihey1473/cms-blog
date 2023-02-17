3634c253e921a909d564edfb7c4b6c35997e05a4
Roy1473 <pgajdmga@gmail.com>
2023-02-17 22:45:32 +0900

    fix

--- src/components/tags/TagList.tsx
+++ src/components/tags/TagList.tsx
@@ -10,13 +10,11 @@ export const TagList: React.FC<{ taggedList: TaggedList }> = (props) => {
   const { id, title, publishedAt } = props.taggedList;
   return (
     <li className={styles.blog_link}>
-      <Link href={`/blog/${id}`}>
-        <a className={styles.blog_link_content}>
-          <time className={publishedAt}>
-            {dayjs(publishedAt).format("YYYY/MM/DD")}
-          </time>
-          <h2 className={styles.blog_link_title}>{title}</h2>
-        </a>
+      <Link href={`/blog/${id}`} className={styles.blog_link_content}>
+        <time className={publishedAt}>
+          {dayjs(publishedAt).format("YYYY/MM/DD")}
+        </time>
+        <h2 className={styles.blog_link_title}>{title}</h2>
       </Link>
     </li>
   );
