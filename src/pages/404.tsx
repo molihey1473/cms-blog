ebf716bf7dfc6fe82dcb9c24160521ed89ad83e0
Roy1473 <pgajdmga@gmail.com>
2023-02-21 00:13:29 +0900

    fix

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,18 +1,13 @@
 import { NextPage } from "next";
 
-import { Wrapper } from "@src/components/Wrapper";
-
 import styles from "@src/styles/pages/Error.module.scss";
 
 const Error: NextPage = () => {
   return (
     <>
       <div className={styles.error_page_layout}>
-        <Wrapper>
-          {" "}
-          <div className={styles.error_status}>404</div>
-          <h1 className={styles.error_message}>Page not found</h1>
-        </Wrapper>
+        <div className={styles.error_status}>404</div>
+        <h1 className={styles.error_message}>Page not found</h1>
       </div>
     </>
   );
