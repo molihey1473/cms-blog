5b80710dd0a40c80c6f371256bea7e56989ae40e
Roy1473 <pgajdmga@gmail.com>
2023-05-02 23:18:55 +0900

    fix

--- src/pages/category/others.tsx
+++ src/pages/category/others.tsx
@@ -1,39 +1,26 @@
 import { NextPage } from "next";
 
-import Head from "next/head";
+import { PageSEO } from "@src/components/PageSEO";
 
-import posts from ".contents/posts.json";
-
-import { Profile } from "@src/components/cards/Profile";
-import { CategoryList } from "@src/components/CategoryList";
-import { PostList } from "@src/components/PostList";
-
-import { member } from "@src/utils/member";
-
-import styles from "@src/styles/components/View.module.scss";
+import { Top } from "@src/features/Top";
+import { CategoryList } from "@src/features/Top/components/CategoryList";
+import { PageView } from "@src/layouts/PageView";
+import { PageWrapper } from "@src/layouts/PageWrapper";
 
 const Page: NextPage = () => {
   return (
     <>
-      <Head>
-        <title>Blog</title>
-        <meta property="og:title" content="MoliHey" />
-        <meta
-          property="og:url"
-          content="https://blog-sage-nine.vercel.app/category/others"
-        />
-        <meta property="og:image" content="ogp/home-ogp.png" />
-        <meta property="og:site_name" content="MoliHey" />
-        <meta name="twitter:card" content="summary_large_image" />
-      </Head>
-      <Profile member={member} />
+      <PageSEO
+        title={"molihey"}
+        path={"/category/others"}
+        isSummaryLarge={true}
+      />
+      <PageView>
+        <PageWrapper>
+          <Top articleListData={[]} pageWithCategory={true} />
+        </PageWrapper>
+      </PageView>
       <CategoryList />
-      <div className={styles.page_others_container}>
-        <h2 className={styles.page_content_title}>Others</h2>
-        <div className={styles.others_items}>
-          <PostList items={posts} />
-        </div>
-      </div>
     </>
   );
 };
