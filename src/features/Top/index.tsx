37ad498de87c7bc60eb46f099f5fb010b23c239d
Roy1473 <pgajdmga@gmail.com>
2023-05-18 23:44:36 +0900

    create: Top component for pages

---
+++ src/features/Top/index.tsx
@@ -0,0 +1,47 @@
+import React, { useCallback, useState } from "react";
+
+import { ArticleItems } from "@src/types/types";
+
+import JsonData from "../../../.contents/posts.json";
+import { ArticleList } from "./components/ArticleList";
+import { RenderCategoryList, RenderTabName } from "./components/CategoryList";
+import { PageTitle } from "./components/PageTitle";
+import { ListContainerView } from "./layouts/ListContainerView";
+export const Top: React.FC<{
+  articleListData: ArticleItems[];
+  pageWithCategory: boolean;
+}> = ({ pageWithCategory }) => {
+  //const { articleListData, pageWithCategory } = props;
+  const [tabCategory, setTabCategory] = useState<string>("All");
+  const handleCategory = useCallback((e) => {
+    setTabCategory(e.currentTarget.textContent);
+  }, []);
+  return (
+    <>
+      <React.StrictMode>
+        <PageTitle title={"All"} />
+        {pageWithCategory ? (
+          <>
+            <RenderCategoryList>
+              <RenderTabName
+                tabName={"All"}
+                tabCategory={tabCategory}
+                onClick={handleCategory}
+              />
+              <RenderTabName
+                tabName={"Other"}
+                tabCategory={tabCategory}
+                onClick={handleCategory}
+              />
+            </RenderCategoryList>
+            <ListContainerView>
+              <ArticleList tabName={tabCategory} renderList={JsonData} />
+            </ListContainerView>
+          </>
+        ) : (
+          <ListContainerView>{tabCategory}</ListContainerView>
+        )}
+      </React.StrictMode>
+    </>
+  );
+};
