c693fec31fdd7343907e9625ba717b16b51fed1b
Roy1473 <pgajdmga@gmail.com>
2023-06-13 22:45:34 +0900

    fix

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 module.exports = {
-  experimental: {
-    optimizeFonts: true,
-  },
+  // experimental: {
+  // optimizeFonts: true,
+  // },
   eslint: {
     dirs: ["src/components/", "src/lib/", "src/pages/", "src/utils/"],
     ignoreDuringBuilds: true,
