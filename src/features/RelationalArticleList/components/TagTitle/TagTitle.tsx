43f266da7f3db66d8470b13417924da9a79a6fd7
Roy1473 <pgajdmga@gmail.com>
2023-07-07 23:55:41 +0900

    fix: view

--- src/features/RelationalArticleList/components/TagTitle/TagTitle.tsx
+++ src/features/RelationalArticleList/components/TagTitle/TagTitle.tsx
@@ -1,9 +1,5 @@
 import styles from "./TagTitle.module.scss";
 
 export const TagTitle: React.FC<{ title: string }> = (props) => {
-  return (
-    <div className={styles.tag_name_container}>
-      <h1 className={styles.tag_name}>{`#${props.title}`}</h1>
-    </div>
-  );
+  return <h1 className={styles.tag_name}>{`#${props.title}`}</h1>;
 };
