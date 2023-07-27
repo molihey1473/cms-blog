916321a1c4755317e75083b39425ed91a47e6411
Roy1473 <pgajdmga@gmail.com>
2023-07-27 14:11:25 +0900

    fix: remove console.log()

--- src/features/Top/components/PageTitle/PageTitle.tsx
+++ src/features/Top/components/PageTitle/PageTitle.tsx
@@ -2,7 +2,6 @@ import { memo } from "react";
 
 import styles from "./PageTitle.module.scss";
 export const PageTitle: React.FC<{ title: string }> = memo((props) => {
-  console.log("PageTitle レンダリング");
   return <h2 className={styles.page_content_title}>{props.title}</h2>;
 });
 PageTitle.displayName = "PageTitle";
