de9a8a8912fb91af1131786e0163786aa87757c8
Roy1473 <pgajdmga@gmail.com>
2023-12-03 03:17:51 +0900

    fix: replace some layout components  from src/pages/tags/[name].tsx

--- src/features/RelationalArticleList/index.tsx
+++ src/features/RelationalArticleList/index.tsx
@@ -1,34 +1,40 @@
 import { ArticleList } from "@src/components/ArticleList";
 import { PageSEO } from "@src/components/PageSEO";
 
+import { PageView } from "@src/layouts/PageView";
+import { PageWrapper } from "@src/layouts/PageWrapper";
+
 import { TagTitle } from "./components/TagTitle";
 import { ListContainerView } from "./layouts/ListContainerView";
 import { TitleContainerView } from "./layouts/TitleContainerView";
-interface Props {
+//interface Props {
+//  name: string;
+//  path: string;
+//  articleList: {
+//    id: string;
+//    title: string;
+//    publishedAt: string;
+//    tags: { name: string }[];
+//  }[];
+//}
+export const RelationalArticleListPage: React.FC<{
   name: string;
   path: string;
-  articleList: {
-    id: string;
-    title: string;
-    publishedAt: string;
-    tags: { name: string }[];
-  }[];
-}
-export const RelationalArticleListPage: React.FC<Props> = (props) => {
-  const { name, path, articleList } = props;
+}> = (props) => {
+  const { name, path } = props;
   return (
     <>
       <PageSEO title={name} path={path} isSummaryLarge={false} />
-      <TitleContainerView>
-        <TagTitle title={name} />
-      </TitleContainerView>
-      <ListContainerView>
-        {articleList.length !== 0 ? (
-          <ArticleList articleListData={articleList} />
-        ) : (
-          <div>関連した記事がありません</div>
-        )}
-      </ListContainerView>
+      <PageView>
+        <PageWrapper>
+          <TitleContainerView>
+            <TagTitle title={name} />
+          </TitleContainerView>
+          <ListContainerView>
+            <ArticleList tagName={name} />
+          </ListContainerView>
+        </PageWrapper>
+      </PageView>
     </>
   );
 };
