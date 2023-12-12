9ea43eaa3e6fd405f38cfbed355a38744976f100
Roy1473 <pgajdmga@gmail.com>
2023-12-13 03:12:00 +0900

    fix: simple component for Top page to make more simple only SSG

--- src/features/Top/index.tsx
+++ src/features/Top/index.tsx
@@ -1,51 +1,69 @@
-import React, { useCallback, useState } from "react";
+import React from "react";
 
 import { ArticleList } from "@src/components/ArticleList";
 import { Profile } from "@src/components/cards/Profile";
-
-import { ArticleItems } from "@src/types/types";
+import { PageSEO } from "@src/components/PageSEO";
 
 import { member } from "@src/utils/member";
 
-import { RenderCategoryList, RenderTabName } from "./components/CategoryList";
-import { PageTitle } from "./components/PageTitle";
+import { PageView } from "@src/layouts/PageView";
+import { PageWrapper } from "@src/layouts/PageWrapper";
+
+//import { RenderCategoryList, RenderTabName } from "./components/CategoryList";
+//import { PageTitle } from "./components/PageTitle";
 import { ListContainerView } from "./layouts/ListContainerView";
-export const Top: React.FC<{
-  articleListData: ArticleItems[];
-  pageWithCategory: boolean;
-}> = ({ pageWithCategory }) => {
-  //const { articleListData, pageWithCategory } = props;
-  const [tabCategory, setTabCategory] = useState<string>("All");
-  const handleCategory = useCallback((e) => {
-    setTabCategory(e.currentTarget.textContent);
-  }, []);
+//
+//export const Top: React.FC<{
+//  pageWithCategory: boolean;
+//}> = ({ pageWithCategory }) => {
+//  const { pageWithCategory } = props;
+//  const [tabCategory, setTabCategory] = useState<string>("All");
+//  const handleCategory = useCallback((e) => {
+//    setTabCategory(e.currentTarget.textContent);
+//  }, []);
+//  return (
+//    <>
+//      <React.StrictMode>
+//        <Profile member={member} />
+//        <PageTitle title={"All"} />
+//        {pageWithCategory ? (
+//          <>
+//            <RenderCategoryList>
+//              <RenderTabName
+//                tabName={"All"}
+//                tabCategory={tabCategory}
+//                onClick={handleCategory}
+//              />
+//              <RenderTabName
+//                tabName={"Other"}
+//                tabCategory={tabCategory}
+//                onClick={handleCategory}
+//              />
+//            </RenderCategoryList>
+//            <ListContainerView>
+//              <ArticleList tagName={undefined} />
+//            </ListContainerView>
+//          </>
+//        ) : (
+//          <ListContainerView>{tabCategory}</ListContainerView>
+//        )}
+//      </React.StrictMode>
+//    </>
+//  );
+//};
+//
+export const TopPage: React.FC = () => {
   return (
     <>
-      <React.StrictMode>
-        <Profile member={member} />
-        <PageTitle title={"All"} />
-        {pageWithCategory ? (
-          <>
-            <RenderCategoryList>
-              <RenderTabName
-                tabName={"All"}
-                tabCategory={tabCategory}
-                onClick={handleCategory}
-              />
-              <RenderTabName
-                tabName={"Other"}
-                tabCategory={tabCategory}
-                onClick={handleCategory}
-              />
-            </RenderCategoryList>
-            <ListContainerView>
-              <ArticleList tagName={undefined} />
-            </ListContainerView>
-          </>
-        ) : (
-          <ListContainerView>{tabCategory}</ListContainerView>
-        )}
-      </React.StrictMode>
+      <PageSEO title={"MoliHey"} isSummaryLarge={false} />
+      <PageView>
+        <PageWrapper>
+          <Profile member={member} />
+          <ListContainerView>
+            <ArticleList tagName={undefined} />
+          </ListContainerView>
+        </PageWrapper>
+      </PageView>
     </>
   );
 };
