b11fa5f5067af90d58a7ca189639fd418c16e97c
Roy1473 <pgajdmga@gmail.com>
2024-01-30 13:24:51 +0900

    fix: rename API_URL path

--- src/utils/blogInfo.ts
+++ src/utils/blogInfo.ts
@@ -1,3 +1,3 @@
-export const BLOG_API = "https://roy1473.microcms.io/api/v1/blog/";
-export const TAG_API = "https://roy1473.microcms.io/api/v1/tags/";
-export const CATEGORY_API = "https://roy1473.microcms.io/api/v1/category/";
+export const BLOG_API_PATH = "https://roy1473.microcms.io/api/v1/blog/";
+export const TAG_API_PATH = "https://roy1473.microcms.io/api/v1/tags/";
+export const CATEGORY_API_PATH = "https://roy1473.microcms.io/api/v1/category/";
